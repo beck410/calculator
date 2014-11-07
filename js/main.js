7b4a0b86393f81442352e25b8b808ebd336d6fea
Alex Story <astory11@yahoo.com>
2014-11-07 16:02:41 -0600

    mostly done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,70 @@
-/* test functions go here */
+$().ready(function(){
+  calc.display = function(arg){
+    if(arg){
+      return $('.display').text(arg);
+    }else {
+      return $('.display').text();
+    }
+  }
+})
+  var calc = {
+
+    total: 0,
+    operator: '',
+    status: false,
+    display: '',
+    strip: function(num){
+      return(parseFloat(num.toPrecision(4)));
+    },
+
+    press: function(input){
+      if(isNaN(+input) === false || input === '.'){
+        if (calc.display().charAt(calc.display().length-1) === '.' && input === '.'){
+          return;
+        }
+        if(calc.status === true){
+          calc.display(' ');
+          calc.status = false;
+        }
+        calc.display(calc.display() + input);
+      } else{
+        if (calc.display().charAt(calc.display().length-1) === '.'){
+          return;
+        }else{
+        switch(input){
+          case '+':
+            calc.total = +calc.total + +calc.display();
+            break;
+          case '-':
+            calc.total = +calc.total - +calc.display();
+            break;
+          case '*':
+            calc.total = +calc.total * +calc.display();
+            break;
+          case '/':
+            calc.total = +calc.total / +calc.display();
+            break;
+          case '=':
+            switch(calc.operator){
+              case '+':
+                calc.total = +calc.total + +calc.display();
+                break;
+              case '-':
+                calc.total = +calc.total - +calc.display();
+                break;
+              case '*':
+                calc.total = +calc.total * +calc.display();
+                break;
+              case '/':
+                calc.total = +calc.total / +calc.display();
+                break;
+              };
+            break;
+          };
+        calc.total;
+        calc.status = true;
+        calc.operator = input;
+        };
+      }
+    }
+  }
--- spec/test.js
+++ spec/test.js
@@ -1,8 +1,35 @@
 /*tests go here */
 describe('input test', function(){
-  it('should print out input\'s value', function(){
-    assert.equal(inputValue("penny"), "penny", "input value is penny");
-    assert.equal(inputValue("jezzy"), "jezzy", "input value is jezzy");
-    assert.equal(inputValue("beck"), "beck", "input value is beck");
+
+  beforeEach(function(){
+    $('.display').text('');
+  })
+
+  it('should add 2 to the display', function(){
+    calc.press('2');
+    assert.equal($('.display').text(), "2");
+  })
+
+  it('should add multiple numbers to the display', function(){
+    calc.press('1');
+    calc.press('2');
+    assert.equal($('.display').text(), '12');
+  })
+
+  it('should disable double decimal', function(){
+    calc.press('1');
+    calc.press('.');
+    calc.press('.');
+    assert.equal($('.display').text(), '1.');
+  })
+
+  it('floating points should be correct', function(){
+    calc.press('.');
+    calc.press('1');
+    calc.press('+');
+    calc.press('.');
+    calc.press('2');
+    calc.press('=');
+    assert.equal($('.display').text(), '.3');
   })
 })
